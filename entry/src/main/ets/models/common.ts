01ea0c389d5a3d6cc9871fba5379c0644c802af5
jellyzhai <1970361237@qq.com>
2024-07-25 01:19:40 +0800

    harmony_app_api12 practice

---
+++ entry/src/main/ets/models/common.ts
@@ -0,0 +1,11 @@
+import { Want } from '@kit.AbilityKit';
+
+export enum HiLogDomainTagEnum {
+  domain = '0x1234',
+  tag = 'test_tag'
+}
+
+export interface WantParametersForResult extends Record<string, Object> {
+  info: string;
+  callerWant: Want
+}
---
+++ entry/src/main/ets/viewModels/index.ts
@@ -0,0 +1,4 @@
+export interface IPageForIndex {
+  title: string;
+  url: string;
+}
